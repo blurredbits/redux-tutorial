1103ed18fdad2129c7da36a8662e6f88020703fe
Blurredbits <mmorris00@mac.com>
2016-07-22 12:58:53 -0600

    thru video 13

--- learn-redux/client/components/single.js
+++ learn-redux/client/components/single.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Photo from './photo';
+import Comments from './comments';
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-      I'm the single page view
-      </div> 
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments}/>
+      </div>
     )
-  } 
+  }
 })
 
 export default Single;
